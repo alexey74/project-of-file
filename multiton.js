07b6b851a56ee8377ac45f6f059a88b10b5184fe
Brendan Graetz <Brendan Graetz>
2015-02-02 16:54:28 +1100

    =BG= allow instrances to use any ID - multiple instances can use the same base if required

--- multiton.js
+++ multiton.js
@@ -4,21 +4,34 @@ var basic = require('./basic');
 
 var instances = {};
 
-function getInstance(base) {
-  var instance = instances[base];
+/*
+ * Gets or retrieves existing instance of a `CachedProjectOfFile`
+ *
+ * @param {string} id: Unique ID for the instance wanted
+ * @param {string} base: The base folder to terminate directory tree traversal
+ */
+function getInstance(id, base) {
+  var instance = instances[id];
   if (!instance) {
     instance = new CachedProjectOfFile(base);
-    instances[base] = instance;
+    instances[id] = instance;
   }
   return instance;
 }
 
+/*
+ * Constructor function for each instance
+ * Caches maps of file to path and file to project name
+ */
 function CachedProjectOfFile(base) {
   this.base = base;
   this.paths = {};
   this.names = {};
 }
 
+/*
+ * @see `./basic.js`
+ */
 CachedProjectOfFile.prototype.path = function path(file) {
   var p = this.paths[file]
   if (!p) {
@@ -28,6 +41,9 @@ CachedProjectOfFile.prototype.path = function path(file) {
   return p;
 };
 
+/*
+ * @see `./basic.js`
+ */
 CachedProjectOfFile.prototype.name = function name(file) {
   var n = this.names[file]
   if (!n) {
