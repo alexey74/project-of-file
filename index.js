03cd349f00eca260990a10ccfda2a7efb39d74e1
csb-cicd-git.gen <csb-cicd-git.gen@cisco.com>
2021-04-05 20:29:15 +0300

    added cli support and dockerfile; updated repo urls

--- index.js
+++ index.js
@@ -5,4 +5,18 @@ var multiton = require('./multiton');
 
 module.exports.path = basic.path;
 module.exports.name = basic.name;
+module.exports.nameVer = basic.nameVer;
 module.exports.cachedInstance = multiton.get;
+
+
+if (require.main === module) {
+    var args = process.argv.slice(2);
+
+    var projectNameVer = basic.nameVer(args[0], args[1]);
+
+    if (projectNameVer) {
+        console.log(projectNameVer[0], projectNameVer[1]);
+    } else {
+        console.log('');
+    }
+}
